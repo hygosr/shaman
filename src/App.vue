a7bcbb16d679f9ebb909452035c72b36de8b5481
hyguinho <hygo.reinaldo@ingka.ikea.com>
2020-09-17 23:42:35 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png" />
+    <img class="football" src="http://upload.wikimedia.org/wikipedia/en/e/ec/Soccer_ball.svg" />
     <PlayerList />
   </div>
 </template>
@@ -22,5 +23,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: whitesmoke;
+}
+
+.football {
+  width: 90px;
+  margin-top: 200px;
+  margin-left: -40px;
 }
 </style>
